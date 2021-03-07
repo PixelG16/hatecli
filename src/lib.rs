224e8ae8170e3459604413babc20d6c88c921199
No3cho <79093085+No3cho@users.noreply.github.com>
2021-03-07 21:36:58 +0000

    Created crate.

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! `hatecli` is a command-line version of [Hatebin](https://hatebin.com).
+
+/// Primary logic of program.
+pub fn run()
+{
+    
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+//! Containing the [`main`] function / entry point to program.
+
+/// Entry point to program.
+///
+/// After parsing values passed via the CLI, it calls the
+/// [`run`](hatecli::run) function which will run the main logic of the 
+/// program.
+fn main() 
+{
+    
+}
