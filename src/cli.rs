e205ae9351fcbeddac053fbf05119fb2bbb4abb8
No3cho <79093085+No3cho@users.noreply.github.com>
2021-03-08 00:32:23 +0000

    Added URL argument.

--- src/cli.rs
+++ src/cli.rs
@@ -4,6 +4,7 @@
 //! a whole and is owner of all arguments, options and subcommands passed via
 //! the CLI.
 
+use url::Url;
 use structopt::StructOpt;
 use std::fs::File;
 
@@ -13,12 +14,29 @@ use std::fs::File;
 pub struct App
 {
     /// Text to be uploaded to Hatebin.
-    #[structopt(short, long, required_unless = "file")]
+    #[structopt(short, 
+                long, 
+                conflicts_with = "file",
+                conflicts_with = "url",
+                required_unless_one(&["file", "url"]))]
     text: Option<String>,
 
     /// File to be uploaded to Hatebin.
-    #[structopt(short, long, conflicts_with = "text", parse(try_from_str = File::open))]
+    #[structopt(short,
+                long, 
+                conflicts_with = "text",
+                conflicts_with = "url",
+                required_unless_one(&["text", "url"]),
+                parse(try_from_str = File::open))]
     file: Option<File>,
+
+    /// Content located at URL to be uploaded to Hatebin.
+    #[structopt(short, 
+                long, 
+                conflicts_with = "file",
+                conflicts_with = "text",
+                required_unless_one(&["file", "text"]))]
+    url: Option<Url>,
 
     /// Force an upload or not, regardless of issues.
     #[structopt(short = "F", long)]
@@ -37,6 +55,12 @@ impl App
     pub fn file(&self) -> &Option<File>
     {
         &self.file
+    }
+
+    /// Content located at URL to be uploaded to Hatebin.
+    pub fn url(&self) -> &Option<Url>
+    {
+        &self.url
     }
 
     /// Force an uploaded or not, regardless of issues.
