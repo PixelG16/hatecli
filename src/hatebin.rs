2611f53d842a6f1742727ef4520bb42585ca3cc7
No3cho <79093085+No3cho@users.noreply.github.com>
2021-03-12 02:01:17 +0000

    No longer post body, instead post form.

--- src/hatebin.rs
+++ src/hatebin.rs
@@ -4,20 +4,20 @@ use reqwest::Error;
 use reqwest::blocking::Client;
 
 /// Maximum amount of characters that can be uploaded before they're truncated.
-pub const CHARACTER_LIMIT: u16 = 50000;
+pub const CHARACTER_LIMIT: usize = 50000;
 
 /// Upload text to [Hatebin](https://hatebin.com).
-pub fn upload(text: String) -> Result<String, Error>
+pub fn upload(text: &str) -> Result<String, Error>
 {
     // Create HTTP Client.
     let client = Client::new();
 
-    // Payload to be posted.
-    let payload = format!("text={}", text);
+    // Form parameters to be posted.
+    let params = [("text", text)];
 
     // Post text.
-    let resp = client.post("https://hatebin.com").body(payload).send()?;
+    let resp = client.post("https://hatebin.com/index.php").form(&params).send()?;
 
     // Return code.
-    Ok(resp.text()?)
+    Ok(resp.text()?.trim().to_string())
 }
